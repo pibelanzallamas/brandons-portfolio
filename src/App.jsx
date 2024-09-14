f54a01f83eb4dfeb9d2f3d2aab88707c5dd4f238
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-09-14 19:56:11 -0300

    sass and title add it

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./app.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className={"ibm-plex-mono-regular"}>
+    <>
       <h1>Brandon Castillo </h1>
       <div>Esta es la pagina de Brandon Castillo.</div>
-    </div>
+    </>
   );
 }
 
