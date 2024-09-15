b3293a63c8b2790d74cf9336f21265ba84192dc5
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-09-14 22:37:43 -0300

    icons add it

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,37 @@ function App() {
     <>
       <h1>Brandon Castillo </h1>
       <div>Esta es la pagina de Brandon Castillo.</div>
+      <div className="redes">
+        <li className="card">
+          <a href="mailto:brandoncastillo.09@gmail.com">
+            <p>brandoncastillo.09@gmail.com</p>
+          </a>
+        </li>
+
+        <li>
+          <a href="https://github.com/pibelanzallamas/" target="_blank">
+            <figure className="redes-icons">
+              <img
+                src="https://pngimg.com/uploads/github/github_PNG23.png"
+                alt="github-logo"
+              ></img>
+            </figure>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/brandon-castillo-dev/"
+            target="_blank"
+          >
+            <figure className="redes-icons">
+              <img
+                src="https://1000marcas.net/wp-content/uploads/2020/01/Logo-Linkedin-500x313.png"
+                alt="likedin-logo"
+              ></img>
+            </figure>
+          </a>
+        </li>
+      </div>
     </>
   );
 }
